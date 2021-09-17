27ea922c49bd3da3706055d3df1d2cbfd61ad76b
Samur Quseinov <sammuurr@gmail.com>
2021-09-17 11:49:41 +0300

    Initial Commit

---
+++ Disign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Disign
+//
+//  Created by KS54 on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
